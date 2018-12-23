07deba3b702a68805a62a96149ebab043c066cc4
Meysam Azad <MeysamAzad81@yahoo.com>
2018-12-23 15:51:41 +0330

    add 2 apps to settings.py, and their urls to urls.py

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from quickstart import views as quickstart_views
+
+router = routers.DefaultRouter()
+router.register('users', quickstart_views.UserViewSet)
+router.register('groups', quickstart_views.GroupViewSet)
 
 urlpatterns = [
+    path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
