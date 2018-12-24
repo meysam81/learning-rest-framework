3baad0ee55fdbe587cf50853dad4a853fc932b69
Meysam Azad <MeysamAzad81@yahoo.com>
2018-12-24 09:32:27 +0330

    add view set instead of explicit views

--- snippets/views.py
+++ snippets/views.py
@@ -1,54 +1,36 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions, renderers
+from rest_framework import generics, permissions, renderers, viewsets
 from django.contrib.auth.models import User
 from .permissions import IsOwnerOrReadOnly, IsCurrentUser
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.order_by('-created')
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
-
-
 @api_view(['GET'])
 def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
+        return Response({
+            'users': reverse('user-list', request=request, format=format),
+            'snippets': reverse('snippet-list', request=request, format=format)
     })
 
 
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.order_by('-created')
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly)
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.order_by('-created')
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.order_by('-date_joined')
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = (IsCurrentUser,)
